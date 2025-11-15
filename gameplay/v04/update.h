069feea18d92519bb1f96db9acf0b0959a6cfd97
Juliana Campos <jucampospinto@gmail.com>
2025-11-15 01:39:36 -0300

    Add files via upload

---
+++ gameplay/v04/update.h
@@ -0,0 +1,8 @@
+#ifndef UPDATE_H
+#define UPDATE_H
+
+#include "defs.h"
+
+void update(Player *player, Enemy enemies[], Bullet bullets[], Bullet enemyBullets[], bool *running, Uint32 *lastEnemySpawnTime, GameState *currentState);
+
+#endif // UPDATE_H
