458d9392794ac48ac1c25375cd8f0b04de8c1b07
Juliana Campos <jucampospinto@gmail.com>
2025-11-16 20:20:27 -0300

    Update input.c

--- gameplay/v04/input.c
+++ gameplay/v04/input.c
@@ -1,20 +1,16 @@
 #include "input.h"
 
-void handleInput(Player *player, bool *running) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            *running = false;
-        }
-        if (event.type == SDL_KEYDOWN) {
-            switch (event.key.keysym.sym) {
-               
-                case SDLK_ESCAPE:
-                    *running = false;
-                    break;
-            }
+void handlePlayingInput_Events(SDL_Event *event, bool *running) {
+    if (event->type == SDL_KEYDOWN) {
+        switch (event->key.keysym.sym) {
+            case SDLK_ESCAPE:
+                *running = false;
+                break;
         }
     }
+}
+
+void handlePlayingInput_State(Player *player) {
 
     const Uint8 *keystates = SDL_GetKeyboardState(NULL);
     
@@ -35,64 +31,53 @@ void handleInput(Player *player, bool *running) {
     }
 }
 
-void handleLevelUpInput (Player *player, GameState *currentState, bool *running) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)){
-        if (event.type == SDL_QUIT) {
-            *running = false;
-        }
-        if (event.type == SDL_KEYDOWN) {
-            bool optionSelected = true;
-            switch (event.key.keysym.sym) {
-                // +HP Máximo
-                case SDLK_1:
-                case SDLK_KP_1:
-                    player->max_hp += 5;
-                    player->hp = player->max_hp; //também cura
-                    printf("Upgrade: HP Máximo +5! \n");
-                    break;
+void handleLevelUpInput (SDL_Event *event, Player *player, GameState *currentState, bool *running) {
+     if (event->type == SDL_KEYDOWN) {
+        bool optionSelected = true;
+        switch (event->key.keysym.sym) {
+            // +HP Máximo
+            case SDLK_1:
+            case SDLK_KP_1:
+                player->max_hp += 5;
+                player->hp = player->max_hp; //também cura
+                printf("Upgrade: HP Máximo +5! \n");
+                break;
 
-                // +Attack Speed
-                case SDLK_2:
-                case SDLK_KP_2:
-                    player->fire_delay = (int)(player->fire_delay * 0.9f);
-                    printf("Upgrade: Cadência de tiro + 10%! \n");
-                    break;
+            // +Attack Speed
+            case SDLK_2:
+            case SDLK_KP_2:
+                player->fire_delay = (int)(player->fire_delay * 0.9f);
+                printf("Upgrade: Cadência de tiro + 10%! \n");
+                break;
 
-                // +Move Speed
-                case SDLK_3:
-                case SDLK_KP_3:
-                    player->speed += 1;
-                    printf("Upgrade: Velocidade de Movimento +1! \n");
-                    break;
+            // +Move Speed
+            case SDLK_3:
+            case SDLK_KP_3:
+                player->speed += 1;
+                printf("Upgrade: Velocidade de Movimento +1! \n");
+                break;
                 
-                default:
-                    optionSelected = false; //ignora qualquer outra tecla
-                    break;
+            default:
+                optionSelected = false; //ignora qualquer outra tecla
+                break;
 
-            }
-            if (optionSelected) {
-                *currentState = STATE_PLAYING;
-            }
         }
+        if (optionSelected) {
+            *currentState = STATE_PLAYING;
+        }
+    
     }
 }
 
-void handleEndInput(GameState *currentState, bool *running) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            *running = false;
-        }
-        if (event.type == SDL_KEYDOWN) {
-            switch (event.key.keysym.sym) {
-                case SDLK_ESCAPE:
-                    *currentState = STATE_QUIT;
-                    break;
-                case SDLK_RETURN: // Tecla Enter
-                    *currentState = STATE_MENU; // Volta ao menu
-                    break;
-            }
+void handleEndInput(SDL_Event *event, GameState *currentState, bool *running) {
+    if (event->type == SDL_KEYDOWN) {
+        switch (event->key.keysym.sym) {
+            case SDLK_ESCAPE:
+                *currentState = STATE_QUIT;
+                break;
+            case SDLK_RETURN:
+                *currentState = STATE_MENU;
+                break;
         }
     }
 }
