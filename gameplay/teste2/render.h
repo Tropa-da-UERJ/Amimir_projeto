344c7bd6c378198ee5ee275e0b9ca869bc08db2e
Juliana Campos <jucampospinto@gmail.com>
2025-11-17 18:09:53 -0300

    Delete gameplay/teste2 directory

--- gameplay/teste2/render.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-#include "defs.h"
-
-void render(SDL_Renderer *renderer, App *app, Player player, Enemy enemies[], Bullet bullets[], Bullet enemyBullets[], GameState currentState);
-
-#endif // RENDER_H
--- gameplay/teste2/text.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEXT_H
-#define TEXT_H
-
-#include "defs.h"
-
-//função para desenhar texto
-void drawText(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, int y, SDL_Color color);
-
-#endif // TEXT_H
