4188f00723b37545dce87201bf8f1a890740d606
Juliana Campos <jucampospinto@gmail.com>
2025-11-25 22:08:33 -0300

    Update menu.h

--- gameplay/v05/menu.h
+++ gameplay/v05/menu.h
@@ -6,5 +6,6 @@ void menu_init(SDL_Renderer *renderer);
 void menu_handle_input(SDL_Event *event, GameState *currentState);
 void menu_render(SDL_Renderer *renderer);
 void menu_destroy(void);
+bool menu_is_in_splash(void);
 
 #endif // MENU_H
