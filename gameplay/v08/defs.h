052bd7f0f7c0475795d48606ae944873ddc13e1a
Juliana Campos <jucampospinto@gmail.com>
2025-12-16 00:36:58 -0300

    Add files via upload

---
+++ gameplay/v08/defs.h
@@ -0,0 +1,137 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+// --- Constantes do Jogo ---
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#define SQUARE_SIZE 30
+
+#define PLAYER_SPEED 5
+#define PLAYER_STARTING_HP 10
+#define PLAYER_FIRE_DELAY 750  // ms entre tiros
+#define PLAYER_BULLET_SPEED 10
+#define PLAYER_MAX_BULLETS 200
+#define PLAYER_XP_TOLEVEL 10
+#define PLAYER_XP_LEVEL_SCALING 1.5f // proximo nivel custa 1.5x(50%) mais
+
+#define ENEMY_BULLET_SIZE 8
+#define PLAYER_BULLET_SIZE 20
+
+#define INVULNERABILITY_DURATION 1000  // 1 segundo
+
+#define MAX_ENEMIES 6
+#define ENEMY_SPAWN_INTERVAL 1500 // em milissegundos
+#define ENEMY_ORIGINAL_SIZE 32
+
+#define ANXIETY_XP_GIVEN 2
+#define ANXIETY_POINTS_GIVEN 10
+#define ANXIETY_STARTING_HP 2
+#define ANXIETY_SPEED 2
+#define ANXIETY_SCALE_FACTOR 2
+
+#define CAFEINA_XP_GIVEN 1
+#define CAFEINA_POINTS_GIVEN 15
+#define CAFEINA_STARTING_HP 1
+#define CAFEINA_SPEED 1
+#define CAFEINA_MAX_BULLETS 200
+#define CAFEINA_BULLET_SPEED 10
+#define CAFEINA_STOP_DISTANCE 300.0f
+#define CAFEINA_FIRE_DELAY 2000
+#define CAFEINA_SCALE_FACTOR 2
+
+#define SEPARATION_STRENGTH 0.3f
+#define MAX_OVERLAP 15
+
+#define ROUND_DURATION_MINUTES 5
+#define ROUND_DURATION_MS (ROUND_DURATION_MINUTES * 60 * 1000) // transforma minutos em milisegundos    
+#define DARKNESS_STEP_MS 60000 // ciclo de escurecimento será de 60 segundos
+
+#define MAX_MAX_DARKNESS_ALPHA 200 // (0 a 255)
+#define SPOTLIGHT_RADIUS 200 // area iluminada ao redor do jogador (em pixels)
+
+// --- Estados do Jogo ---
+typedef enum {
+    STATE_SPLASH,
+    STATE_MENU,
+    STATE_PLAYING,
+    STATE_OPTIONS,
+    STATE_LEVELUP,
+    STATE_END,
+    STATE_PAUSED,
+    STATE_QUIT,
+    STATE_SCOREBOARD
+} GameState;
+
+// --- Estruturas de Dados ---
+
+typedef struct {
+    SDL_Rect rect;
+    int dx, dy;
+    int hp;
+    int max_hp; // para o levelup
+    Uint32 lastShotTime;
+    bool invulnerable;
+    Uint32 invulnerableTime;
+    int xp;
+    int xp_toLevel;
+    int points;
+    float speed;
+    float fire_delay;
+    SDL_Texture *texture;
+    int frameX;
+    int frameY;
+} Player;
+
+typedef struct {
+    SDL_Rect rect;
+    bool active;
+    int hp;
+    int points_given;
+    int xp_given;
+    int type; // 0 para ANSIEDADE e 1 para CAFEINA
+    int speed;
+    Uint32 lastShotTime;
+} Enemy;
+
+typedef struct {
+    SDL_Rect rect;
+    int dx, dy;
+    bool active;
+} Bullet;
+
+typedef struct {
+    TTF_Font *font;
+    SDL_Texture *texCafeina;
+    SDL_Texture *texAnsiedade;
+    SDL_Texture *texBullet;
+
+    //iluminação
+    int currentDarknessAlpha;
+    SDL_Texture *texDarknessLayer; //camada preta que cobre a tela
+    SDL_Texture *texSpotlight; // "forma" da luz (circulo)
+    
+    //timer
+    Uint32 roundStartTime;
+    Uint32 elapsedTime;
+
+} App;
+
+typedef enum {
+    MENU_BUTTON_PLAY,
+    MENU_BUTTON_OPTIONS,
+    MENU_BUTTON_QUIT,
+    MENU_BUTTON_COUNT // Usado para saber quantos botões temos
+} MenuButton;
+
+typedef enum {
+    END_BUTTON_PLAYAGAIN,
+    END_BUTTON_MENUAGAIN,
+    END_BUTTON_COUNT 
+} EndButton;
+
+#endif // DEFS_H
---
+++ gameplay/v08/render.c
@@ -0,0 +1,194 @@
+#include "render.h"
+#include "defs.h"
+#include "menu.h"
+#include "text.h"
+#include "fim.h"
+
+
+// Cores que usaremos
+SDL_Color white = {255, 255, 255, 255};
+SDL_Color black = {0, 0, 0, 255};
+SDL_Color red = {255, 0, 0, 255};
+SDL_Color green = {0, 255, 0, 255};
+SDL_Color blue = {0, 0, 255, 255};
+SDL_Color yellow = {255, 255, 0, 255};
+
+void render(SDL_Renderer * renderer, App * app, Player player, Enemy enemies[], Bullet bullets[], Bullet enemyBullets[], GameState currentState) {
+
+  // Desenha o Jogo (STATE_PLAYING ou STATE_LEVELUP) ---
+  if (currentState == STATE_PLAYING || currentState == STATE_LEVELUP) {
+
+    // Limpa a tela (fundo azul)
+    SDL_SetRenderDrawColor(renderer, 50, 161, 157, 255);
+    SDL_RenderClear(renderer);
+
+    // Desenha o Jogador
+    if (player.texture != NULL) {
+        Uint32 now = SDL_GetTicks();
+        SDL_Rect srcRect;
+        srcRect.x = player.frameX;
+        srcRect.y = 0; // sheet horizontal
+        srcRect.w = 32;
+        srcRect.h = 32;
+
+        if (player.invulnerable) {
+            // lógica de piscar
+            if ((now / 200) % 2 == 0) {
+                 SDL_RenderCopy(renderer, player.texture, &srcRect, &player.rect); 
+            }
+        } else {
+            SDL_RenderCopy(renderer, player.texture, &srcRect, &player.rect);
+        }
+    } else {
+        // Fallback: Se o sprite não carregou, desenha o retângulo roxo padrão
+        SDL_SetRenderDrawColor(renderer, 128, 0, 128, 255); 
+        SDL_RenderFillRect(renderer, &player.rect);
+    }
+
+    // Desenha os Inimigos
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+      if (enemies[i].active) {
+        if (enemies[i].type == 0) {
+          if (app->texAnsiedade != NULL) {
+            // Desenha a sprite no retângulo que já tem o tamanho correto (escalado)
+            SDL_RenderCopy(renderer, app->texAnsiedade, NULL, &enemies[i].rect);
+          } else {
+            // Fallback (Mantém o quadrado vermelho se a textura falhar)
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); 
+            SDL_RenderFillRect(renderer, &enemies[i].rect);
+          }
+        }
+        else {
+          if (app->texCafeina != NULL) {
+                // Desenha a imagem esticada no tamanho do retângulo do inimigo
+                SDL_RenderCopy(renderer, app->texCafeina, NULL, &enemies[i].rect);
+          } else {
+              SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); 
+              SDL_RenderFillRect(renderer, &enemies[i].rect);
+          }
+        }
+      }
+    }
+
+    // Desenha os Projéteis (Branco)
+    //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for (int i = 0; i < PLAYER_MAX_BULLETS; i++) {
+      if (bullets[i].active) {
+        if (app->texBullet != NULL) {
+            // Desenha a textura do Z
+            SDL_RenderCopy(renderer, app->texBullet, NULL, &bullets[i].rect);
+        } else {
+            // Fallback: Se a textura falhar, desenha o quadrado antigo
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderFillRect(renderer, &bullets[i].rect);
+        }
+      }  
+    }
+
+    // Desenha os Projéteis Inimigos (pretos)
+    for (int i = 0; i < CAFEINA_MAX_BULLETS; i++) {
+      if (enemyBullets[i].active) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // preto
+        SDL_RenderFillRect(renderer, &enemyBullets[i].rect);
+      }
+    }
+
+    if (app->texDarknessLayer != NULL && app->texSpotlight != NULL) {
+        
+        // Define a texture de luz como o ALVO
+        SDL_SetRenderTarget(renderer, app->texDarknessLayer);
+
+        // Calcula a luz ambiente baseada no timer
+        
+        int ambientLight = 255 - app->currentDarknessAlpha; 
+        if (ambientLight < 20) ambientLight = 20; // Limite mínimo de escuridão total
+
+        // Limpa a tela com a cor do ambiente (Cinza Escuro se for noite, Branco se for dia)
+        SDL_SetRenderDrawColor(renderer, ambientLight, ambientLight, ambientLight, 255);
+        SDL_RenderClear(renderer);
+
+        // Desenha a "Lanterna" (soma luz branca) onde o player está  
+        SDL_Rect lightRect;
+
+        lightRect.w = SPOTLIGHT_RADIUS * 2; 
+        lightRect.h = SPOTLIGHT_RADIUS * 2; 
+        lightRect.x = player.rect.x + (player.rect.w / 2) - (lightRect.w / 2);
+        lightRect.y = player.rect.y + (player.rect.h / 2) - (lightRect.h / 2);
+
+        // Desenha a luz apagando a escuridão
+        SDL_RenderCopy(renderer, app->texSpotlight, NULL, &lightRect);
+
+        // Reseta o ALVO para a tela padrão
+        SDL_SetRenderTarget(renderer, NULL);
+
+        // Desenha a camada de escuridão pronta por cima do jogo
+        SDL_RenderCopy(renderer, app->texDarknessLayer, NULL, NULL);
+    }
+
+    // Desenhar a HUD (Health, Points, XP) ---
+    if (app -> font) { // Só desenha se a fonte foi carregada
+      char hudText[150];
+
+      // 1. HUD Superior Esquerda (Vida e Pontos)
+      sprintf(hudText, "HP: %d/%d | PONTOS: %d", player.hp, player.max_hp, player.points);
+      drawText(renderer, app -> font, hudText, 10, 10, white);
+
+      // 2. HUD Superior Direita (XP para o Próximo Nível)
+      sprintf(hudText, "XP: %d / %d", player.xp, player.xp_toLevel);
+      int textX = SCREEN_WIDTH - 200; // Começa 200 pixels da direita
+      drawText(renderer, app -> font, hudText, textX, 10, yellow);
+
+      // 3. HUD Superior Central (Timer)
+      int remainingMs = ROUND_DURATION_MS - app->elapsedTime;
+
+      if (remainingMs < 0)
+        remainingMs = 0;
+
+      int minutes = remainingMs / 60000;
+      int seconds = (remainingMs % 60000) / 1000;
+
+      char timerText[20]; 
+      sprintf(timerText, "%d:%02d", minutes, seconds);
+      drawText(renderer, app -> font, timerText, (SCREEN_WIDTH / 2) - 50, 10, yellow);
+    }
+  }
+  // --- 2. Desenha o Menu (STATE_MENU) ---
+  else if (currentState == STATE_MENU) {
+    menu_render(renderer);
+
+    if (menu_is_in_splash()) { // função auxiliar em menu.c/h
+      Uint32 now = SDL_GetTicks();
+      if ((now % 1000) < 500) { // pisca a cada 1 segundo (500ms on, 500ms off)
+        const char *flashText = "Pressione qualquer tecla para continuar";
+        drawText(renderer, app->font, flashText, 200, 400, white);
+      }
+    }
+  }
+
+    // --- 3. Desenha o Fim de Jogo (STATE_END) ---
+  else if (currentState == STATE_END) {
+    fim_render(renderer, player, app);
+  }
+
+  // --- 4. Desenha a Tela de Level Up (POR CIMA de STATE_PLAYING) ---
+  if (currentState == STATE_LEVELUP) {
+    //escurecer a tela
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND); // blend permite transparencia
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 150); // cor preta semi-transparente
+
+    SDL_Rect screenRect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+    SDL_RenderFillRect(renderer, & screenRect);
+
+    // Desenha as opções (no centro da tela)
+    if (app -> font) {
+      drawText(renderer, app -> font, "--- LEVEL UP! ---", 250, 150, white);
+      drawText(renderer, app -> font, "Escolha seu upgrade! (Aperte 1, 2 ou 3...)", 250, 200, white);
+
+      drawText(renderer, app -> font, "1. HP Maximo +5", 250, 250, red);
+      drawText(renderer, app -> font, "2. Cadencia de Tiro +10%", 250, 280, red);
+      drawText(renderer, app -> font, "3. Velocidade de Movimento +1", 250, 310, red);
+    }
+  }
+  SDL_RenderPresent(renderer);
+  }
