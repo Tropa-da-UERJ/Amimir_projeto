836bdd37a050b27427ca80fc7a0228675ca78140
Juliana Campos <jucampospinto@gmail.com>
2025-12-17 11:51:56 -0300

    Delete gameplay/v09 directory

--- gameplay/v09/update.h
+++
@@ -1,8 +0,0 @@
-#ifndef UPDATE_H
-#define UPDATE_H
-
-#include "defs.h"
-
-void update(Player *player, Enemy enemies[], Bullet bullets[], Bullet enemyBullets[], bool *running, Uint32 *lastEnemySpawnTime, GameState *currentState, App *app);
-
-#endif // UPDATE_H
